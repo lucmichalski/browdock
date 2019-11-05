86dd69560873160a7a457d87492950b3deee671f
Axel Foucheriq <afoucheriq@cleonet.fr>
2019-11-05 16:50:26 +0100

    tests with shm

--- Brave/run.sh
+++ Brave/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker container run -m 4g -it --rm --security-opt seccomp=$(pwd)/chrome.json --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" brave-docker
+docker container run -m 4g -it --rm --security-opt seccomp=$(pwd)/chrome.json --net=host --env="DISPLAY" -v /dev/shm:/dev/shm --volume="$HOME/.Xauthority:/root/.Xauthority:rw" brave-docker
--- Firefox/run.sh
+++ Firefox/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -m 6g --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" firefox-docker
+docker run -m 6g --net=host --env="DISPLAY" -v /dev/shm:/dev/shm --volume="$HOME/.Xauthority:/root/.Xauthority:rw" firefox-docker
